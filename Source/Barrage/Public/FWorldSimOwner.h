310aa8341180e8c8c72690142cfa26b6ad82b242
JKurzer <jake.kurzer@gmail.com>
2024-08-05 20:33:00 -0400

    Initial sketch of a world owner.
    No concept of adding bodies yet.

--- Source/Barrage/Public/FWorldSimOwner.h
+++ Source/Barrage/Public/FWorldSimOwner.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
-//unity build isn't fond of this.
+//unity build isn't fond of this, but we really want to completely contain these types and also prevent any collisions.
+//there's other ways to do this, but the correct way is a namespace so far as I know.
 // see: https://dev.epicgames.com/documentation/en-us/unreal-engine/epic-cplusplus-coding-standard-for-unreal-engine?application_version=5.4#namespaces
 namespace Barrage
 {
@@ -60,7 +61,11 @@ namespace Barrage
 
 	// We're also using STL classes in this example
 	using namespace std;
-
+	
+	// Layer that objects can be in, determines which other objects it can collide with
+	// Typically you at least want to have 1 layer for moving bodies and 1 layer for static bodies, but you can have more
+	// layers if you want. E.g. you could have a layer for high detail collision (which is not used by the physics simulation
+	// but only if you do collision testing).
 	namespace Layers
 	{
 		static constexpr ObjectLayer NON_MOVING = 0;
@@ -102,17 +107,6 @@ namespace Barrage
 
 	class FWorldSimOwner
 	{
-
-
-
-		// Layer that objects can be in, determines which other objects it can collide with
-		// Typically you at least want to have 1 layer for moving bodies and 1 layer for static bodies, but you can have more
-		// layers if you want. E.g. you could have a layer for high detail collision (which is not used by the physics simulation
-		// but only if you do collision testing).
-
-
-		// BroadPhaseLayerInterface implementation
-		// This defines a mapping between object and broadphase layers.
 		class BPLayerInterfaceImpl final : public BroadPhaseLayerInterface
 		{
 		public:
@@ -215,10 +209,59 @@ namespace Barrage
 				
 			}
 		};
+		
+	public:
+		PhysicsSystem physics_system;
+		
+		TSharedPtr<JobSystemThreadPool> job_system;
+		// Create mapping table from object layer to broadphase layer
+		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
+		BPLayerInterfaceImpl broad_phase_layer_interface;
+
+		// Create class that filters object vs broadphase layers
+		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
+		ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter;
+
+		// Create class that filters object vs object layers
+		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
+		ObjectLayerPairFilterImpl object_vs_object_layer_filter;
 
 
-		FWorldSimOwner()
+		// A contact listener gets notified when bodies (are about to) collide, and when they separate again.
+		// Note that this is called from a job so whatever you do here needs to be thread safe.
+		// Registering one is entirely optional.
+		MyContactListener contact_listener;
+
+		// A body activation listener gets notified when bodies activate and go to sleep
+		// Note that this is called from a job so whatever you do here needs to be thread safe.
+		// Registering one is entirely optional.
+		MyBodyActivationListener body_activation_listener;
+		
+		float DeltaTime = 0.01;//You should set this!
+
+		BodyInterface* body_interface;
+
+		// This is the max amount of rigid bodies that you can add to the physics system. If you try to add more you'll get an error.
+		// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
+		const uint cMaxBodies = 1024;
+
+		// This determines how many mutexes to allocate to protect rigid bodies from concurrent access. Set it to 0 for the default settings.
+		const uint cNumBodyMutexes = 0;
+
+		// This is the max amount of body pairs that can be queued at any time (the broad phase will detect overlapping
+		// body pairs based on their bounding boxes and will insert them into a queue for the narrowphase). If you make this buffer
+		// too small the queue will fill up and the broad phase jobs will start to do narrow phase work. This is slightly less efficient.
+		// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
+		const uint cMaxBodyPairs = 1024;
+
+		// This is the maximum size of the contact constraint buffer. If more contacts (collisions between bodies) are detected than this
+		// number then these contacts will be ignored and bodies will start interpenetrating / fall through the world.
+		// Note: This value is low because this is a simple test. For a real project use something in the order of 10240.
+		const uint cMaxContactConstraints = 1024;
+		
+		FWorldSimOwner(float cDeltaTime)
 		{
+			DeltaTime = cDeltaTime;
 			// Register allocation hook. In this example we'll just let Jolt use malloc / free but you can override these if you want (see Memory.h).
 			// This needs to be done before any other Jolt function is called.
 			RegisterDefaultAllocator();
@@ -241,137 +284,96 @@ namespace Barrage
 			// B.t.w. 10 MB is way too much for this example but it is a typical value you can use.
 			// If you don't want to pre-allocate you can also use TempAllocatorMalloc to fall back to
 			// malloc / free.
-			TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
+			
 
 			// We need a job system that will execute physics jobs on multiple threads. Typically
 			// you would implement the JobSystem interface yourself and let Jolt Physics run on top
 			// of your own job scheduler. JobSystemThreadPool is an example implementation.
-			JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1);
-
-			// This is the max amount of rigid bodies that you can add to the physics system. If you try to add more you'll get an error.
-			// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
-			const uint cMaxBodies = 1024;
-
-			// This determines how many mutexes to allocate to protect rigid bodies from concurrent access. Set it to 0 for the default settings.
-			const uint cNumBodyMutexes = 0;
-
-			// This is the max amount of body pairs that can be queued at any time (the broad phase will detect overlapping
-			// body pairs based on their bounding boxes and will insert them into a queue for the narrowphase). If you make this buffer
-			// too small the queue will fill up and the broad phase jobs will start to do narrow phase work. This is slightly less efficient.
-			// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
-			const uint cMaxBodyPairs = 1024;
-
-			// This is the maximum size of the contact constraint buffer. If more contacts (collisions between bodies) are detected than this
-			// number then these contacts will be ignored and bodies will start interpenetrating / fall through the world.
-			// Note: This value is low because this is a simple test. For a real project use something in the order of 10240.
-			const uint cMaxContactConstraints = 1024;
-
-			// Create mapping table from object layer to broadphase layer
-			// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
-			BPLayerInterfaceImpl broad_phase_layer_interface;
-
-			// Create class that filters object vs broadphase layers
-			// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
-			ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter;
-
-			// Create class that filters object vs object layers
-			// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
-			ObjectLayerPairFilterImpl object_vs_object_layer_filter;
-
+			job_system = MakeShareable(new JobSystemThreadPool(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1));
 			// Now we can create the actual physics system.
-
-			PhysicsSystem physics_system;
 			physics_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints,
 			                    broad_phase_layer_interface, object_vs_broadphase_layer_filter,
 			                    object_vs_object_layer_filter);
 
-			// A body activation listener gets notified when bodies activate and go to sleep
-			// Note that this is called from a job so whatever you do here needs to be thread safe.
-			// Registering one is entirely optional.
-			MyBodyActivationListener body_activation_listener;
+			
 			physics_system.SetBodyActivationListener(&body_activation_listener);
 
-			// A contact listener gets notified when bodies (are about to) collide, and when they separate again.
-			// Note that this is called from a job so whatever you do here needs to be thread safe.
-			// Registering one is entirely optional.
-			MyContactListener contact_listener;
+
 			physics_system.SetContactListener(&contact_listener);
 
 			// The main way to interact with the bodies in the physics system is through the body interface. There is a locking and a non-locking
 			// variant of this. We're going to use the locking version (even though we're not planning to access bodies from multiple threads)
-			BodyInterface& body_interface = physics_system.GetBodyInterface();
+			body_interface = &physics_system.GetBodyInterface();
 
 			// Next we can create a rigid body to serve as the floor, we make a large box
 			// Create the settings for the collision volume (the shape).
 			// Note that for simple shapes (like boxes) you can also directly construct a BoxShape.
-			BoxShapeSettings floor_shape_settings(Vec3(100.0f, 1.0f, 100.0f));
-			floor_shape_settings.SetEmbedded();
+			// BoxShapeSettings floor_shape_settings(Vec3(100.0f, 1.0f, 100.0f));
+			// floor_shape_settings.SetEmbedded();
 			// A ref counted object on the stack (base class RefTarget) should be marked as such to prevent it from being freed when its reference count goes to 0.
 
 			// Create the shape
-			ShapeSettings::ShapeResult floor_shape_result = floor_shape_settings.Create();
-			ShapeRefC floor_shape = floor_shape_result.Get();
+			//ShapeSettings::ShapeResult floor_shape_result = floor_shape_settings.Create();
+			//ShapeRefC floor_shape = floor_shape_result.Get();
 			// We don't expect an error here, but you can check floor_shape_result for HasError() / GetError()
 
 			// Create the settings for the body itself. Note that here you can also set other properties like the restitution / friction.
-			BodyCreationSettings floor_settings(floor_shape, RVec3(0.0_r, -1.0_r, 0.0_r), Quat::sIdentity(),
-			                                    EMotionType::Static, Layers::NON_MOVING);
+			//BodyCreationSettings floor_settings(floor_shape, RVec3(0.0_r, -1.0_r, 0.0_r), Quat::sIdentity(),
+			// EMotionType::Static, Layers::NON_MOVING);
 
 			// Create the actual rigid body
-			Body* floor = body_interface.CreateBody(floor_settings);
+			//Body* floor = body_interface.CreateBody(floor_settings);
 			// Note that if we run out of bodies this can return nullptr
 
 			// Add it to the world
-			body_interface.AddBody(floor->GetID(), EActivation::DontActivate);
+			//body_interface.AddBody(floor->GetID(), EActivation::DontActivate);
 
 			// Now create a dynamic body to bounce on the floor
 			// Note that this uses the shorthand version of creating and adding a body to the world
-			BodyCreationSettings sphere_settings(new SphereShape(0.5f), RVec3(0.0_r, 2.0_r, 0.0_r), Quat::sIdentity(),
-			                                     EMotionType::Dynamic, Layers::MOVING);
-			BodyID sphere_id = body_interface.CreateAndAddBody(sphere_settings, EActivation::Activate);
+			//BodyCreationSettings sphere_settings(new SphereShape(0.5f), RVec3(0.0_r, 2.0_r, 0.0_r), Quat::sIdentity(),
+			//                                     EMotionType::Dynamic, Layers::MOVING);
+			//BodyID sphere_id = body_interface.CreateAndAddBody(sphere_settings, EActivation::Activate);
 
 			// Now you can interact with the dynamic body, in this case we're going to give it a velocity.
 			// (note that if we had used CreateBody then we could have set the velocity straight on the body before adding it to the physics system)
-			body_interface.SetLinearVelocity(sphere_id, Vec3(0.0f, -5.0f, 0.0f));
+			//body_interface.SetLinearVelocity(sphere_id, Vec3(0.0f, -5.0f, 0.0f));
 
 			// We simulate the physics world in discrete time steps. 60 Hz is a good rate to update the physics system.
-			const float cDeltaTime = 1.0f / 60.0f;
+			
 
+
+
+			// Remove the sphere from the physics system. Note that the sphere itself keeps all of its state and can be re-added at any time.
+			// body_interface.RemoveBody(sphere_id);
+
+			// Destroy the sphere. After this the sphere ID is no longer valid.
+			// body_interface.DestroyBody(sphere_id);
+
+			// Remove and destroy the floor
+			// body_interface.RemoveBody(floor->GetID());
+			// body_interface.DestroyBody(floor->GetID());
+
+			// Unregisters all types with the factory and cleans up the default material
+
+		}
+
+		void StepSimulation()
+		{
 			// Optional step: Before starting the physics simulation you can optimize the broad phase. This improves collision detection performance (it's pointless here because we only have 2 bodies).
 			// You should definitely not call this every frame or when e.g. streaming in a new level section as it is an expensive operation.
 			// Instead insert all new objects in batches instead of 1 at a time to keep the broad phase efficient.
 			physics_system.OptimizeBroadPhase();
 
-			// Now we're ready to simulate the body, keep simulating until it goes to sleep
-			uint step = 0;
-			while (body_interface.IsActive(sphere_id))
-			{
-				// Next step
-				++step;
 
-				// Output current position and velocity of the sphere
-				RVec3 position = body_interface.GetCenterOfMassPosition(sphere_id);
-				Vec3 velocity = body_interface.GetLinearVelocity(sphere_id);
+			// If you take larger steps than 1 / 60th of a second you need to do multiple collision steps in order to keep the simulation stable. Do 1 collision step per 1 / 60th of a second (round up).
+			const int cCollisionSteps = 1;
 
-				//readd debug here.
-				// If you take larger steps than 1 / 60th of a second you need to do multiple collision steps in order to keep the simulation stable. Do 1 collision step per 1 / 60th of a second (round up).
-				const int cCollisionSteps = 1;
-
-				// Step the world
-				physics_system.Update(cDeltaTime, cCollisionSteps, &temp_allocator, &job_system);
-			}
-
-			// Remove the sphere from the physics system. Note that the sphere itself keeps all of its state and can be re-added at any time.
-			body_interface.RemoveBody(sphere_id);
-
-			// Destroy the sphere. After this the sphere ID is no longer valid.
-			body_interface.DestroyBody(sphere_id);
-
-			// Remove and destroy the floor
-			body_interface.RemoveBody(floor->GetID());
-			body_interface.DestroyBody(floor->GetID());
-
-			// Unregisters all types with the factory and cleans up the default material
+			// Step the world
+			physics_system.Update(DeltaTime, cCollisionSteps, &temp_allocator, &job_system);
+		}
+		
+		~FWorldSimOwner()
+		{
 			UnregisterTypes();
     
 			// Destroy the factory
