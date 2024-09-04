fc8f06f7dd426369b32601a106c715518218ef02
JKurzer <jake.kurzer@gmail.com>
2024-09-04 15:03:36 -0400

    reformat.

--- Source/Barrage/Public/PhysicsCharacter.h
+++ Source/Barrage/Public/PhysicsCharacter.h
@@ -15,61 +15,65 @@ namespace JOLT
 {
 	// this will get hidden in the cpp prolly to some extent.
 	// you might see some use of jolt coding conventions when working in the barrage codebase.
-	
+
 	class FJoltCharacter
 	{
 	public:
-		JPH::RVec3				mInitialPosition = JPH::RVec3::sZero();
-		float					mHeightStanding = 1.35f;
-		float					mRadiusStanding = 0.3f;
+		JPH::RVec3 mInitialPosition = JPH::RVec3::sZero();
+		float mHeightStanding = 1.35f;
+		float mRadiusStanding = 0.3f;
 		JPH::CharacterVirtualSettings mCharacterSettings;
 		CharacterVirtual::ExtendedUpdateSettings mUpdateSettings;
 		// Character movement settings (update to control the movement of the character)
-		Vec3					mHorizontalSpeed = Vec3::sZero();
-		float					mJumpSpeed = 0.0f; // Character will jump when not 0, will auto reset
-		Ref<CharacterVirtual>	mCharacter;
-		float					mDeltaTime;
+		Vec3 mHorizontalSpeed = Vec3::sZero();
+		float mJumpSpeed = 0.0f; // Character will jump when not 0, will auto reset
+		Ref<CharacterVirtual> mCharacter;
+		float mDeltaTime;
 
 		// Calculated effective velocity after a step
-		Vec3					mEffectiveVelocity = Vec3::sZero();
+		Vec3 mEffectiveVelocity = Vec3::sZero();
+
 	protected:
 		friend class FWorldSimOwner;
 		TSharedPtr<FWorldSimOwner> Machine;
-		TSharedPtr<JPH::PhysicsSystem,ESPMode::ThreadSafe> World;
+		TSharedPtr<JPH::PhysicsSystem, ESPMode::ThreadSafe> World;
 
 	public:
-		RVec3					GetPosition() const
+		RVec3 GetPosition() const
 		{
 			return mCharacter->GetPosition();
 		}
 
 		// Create the character
-		void					Create()
+		void Create()
 		{
 			// Create capsule
 			auto HoldOpen = Machine;
 			World = HoldOpen->physics_system; // this MAY lead to a circ ref?  I think I've avoided it.
 			Ref<Shape> capsule = new CapsuleShape(0.5f * mHeightStanding, mRadiusStanding);
-			mCharacterSettings.mShape = RotatedTranslatedShapeSettings(Vec3(0, 0.5f * mHeightStanding + mRadiusStanding, 0), Quat::sIdentity(), capsule).Create().Get();
+			mCharacterSettings.mShape = RotatedTranslatedShapeSettings(
+				Vec3(0, 0.5f * mHeightStanding + mRadiusStanding, 0), Quat::sIdentity(), capsule).Create().Get();
 
 			// Configure supporting volume
-			mCharacterSettings.mSupportingVolume = Plane(Vec3::sAxisY(), -mHeightStanding); // Accept contacts that touch the lower sphere of the capsule
+			mCharacterSettings.mSupportingVolume = Plane(Vec3::sAxisY(), -mHeightStanding);
+			// Accept contacts that touch the lower sphere of the capsule
 
 			// Create character
 			mCharacter = new CharacterVirtual(&mCharacterSettings, mInitialPosition, Quat::sIdentity(), 0, World.Get());
 			//mCharacter->SetListener(this);
 			mCharacter->SetCharacterVsCharacterCollision(&Machine->mCharacterVsCharacterCollision);
 		}
-		void					Step()
+
+		void StepCharacter()
 		{
-			
 			// Determine new basic velocity
 			Vec3 current_vertical_velocity = Vec3(0, mCharacter->GetLinearVelocity().GetY(), 0);
 			Vec3 ground_velocity = mCharacter->GetGroundVelocity();
 			Vec3 new_velocity;
-			
+
 			if (mCharacter->GetGroundState() == CharacterVirtual::EGroundState::OnGround // If on ground
-				&& (current_vertical_velocity.GetY() - ground_velocity.GetY()) < 0.1f) // And not moving away from ground
+				&& (current_vertical_velocity.GetY() - ground_velocity.GetY()) < 0.1f)
+			// And not moving away from ground
 			{
 				// Assume velocity of ground when on ground
 				new_velocity = ground_velocity;
@@ -79,7 +83,9 @@ namespace JOLT
 				mJumpSpeed = 0.0f;
 			}
 			else
+			{
 				new_velocity = current_vertical_velocity;
+			}
 
 			// Gravity
 			new_velocity += World->GetGravity() * mDeltaTime;
@@ -95,17 +101,16 @@ namespace JOLT
 			// Update the character position
 			TempAllocatorMalloc allocator;
 			mCharacter->ExtendedUpdate(mDeltaTime,
-				World->GetGravity(),
-				mUpdateSettings,
-				World->GetDefaultBroadPhaseLayerFilter(Layers::MOVING),
-				World->GetDefaultLayerFilter(Layers::MOVING),
-				{ },
-				{ },
-				allocator);
+			                           World->GetGravity(),
+			                           mUpdateSettings,
+			                           World->GetDefaultBroadPhaseLayerFilter(Layers::MOVING),
+			                           World->GetDefaultLayerFilter(Layers::MOVING),
+			                           {},
+			                           {},
+			                           allocator);
 
 			// Calculate effective velocity in this step
 			mEffectiveVelocity = Vec3(GetPosition() - start_pos) / mDeltaTime;
 		}
-		
 	};
 }
