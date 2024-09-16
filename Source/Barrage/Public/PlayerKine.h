e2ee043a2878289f034b070467016df51f713067
JKurzer <jake.kurzer@gmail.com>
2024-09-15 20:39:33 -0400

    Add Player Kine!

--- Source/Barrage/Public/PlayerKine.h
+++ Source/Barrage/Public/PlayerKine.h
@@ -1,3 +1,47 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "Kines.h"
+
+class PlayerKine : public ActorKine
+{
+	//static inline FActorComponentTickFunction FALSEHOOD_MALEVOLENCE_TRICKERY = FActorComponentTickFunction();
+public:
+	TWeakObjectPtr<AActor> MySelf;
+	
+	explicit PlayerKine(const TWeakObjectPtr<AActor>& MySelf, const ActorKey& Target)
+		: MySelf(MySelf)
+	{
+		MyKey = Target;
+	}
+
+	virtual void SetLocationAndRotation(FVector3d Loc, FQuat4d Rot) override
+	{
+		TObjectPtr<AActor> Pin;
+		Pin = MySelf.Get();
+		if(Pin)
+		{
+			Pin->SetActorLocationAndRotation(Loc, Rot);
+		}
+	}
+
+	virtual void SetLocation(FVector3d Location) override
+	{
+		TObjectPtr<AActor> Pin;
+		Pin = MySelf.Get();
+		if(Pin)
+		{
+			Pin->SetActorLocation(Location, false, nullptr, ETeleportType::None);
+		}
+	}
+
+	virtual void SetRotation(FQuat4d Rotation) override
+	{
+		TObjectPtr<AActor> Pin;
+		Pin = MySelf.Get();
+		if(Pin)
+		{
+			Pin->SetActorRotation(Rotation, ETeleportType::None);
+		}
+	}
+};
