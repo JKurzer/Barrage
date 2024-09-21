0ffb850aebab7f65553a19d3fd085cfa60827f8a
JKurzer <jake.kurzer@gmail.com>
2024-09-21 05:03:14 -0400

    math is now working
    ratio's not correct, unfortunately, it looks like, or some aspect of delta t isn't being correctly applied but overall, this is a marked improvement. we're still not seeing movement, but oddly, the vector state of the effective velocity vector is updating. I wonder if the player kine is still not quite right?

--- Source/Barrage/Private/PhysicsCharacter.cpp
+++ Source/Barrage/Private/PhysicsCharacter.cpp
@@ -29,21 +29,34 @@ JPH::BodyID JOLT::FBCharacter::Create(CharacterVsCharacterCollision* CVCCollider
 void JOLT::FBCharacter::StepCharacter()
 {
 	// Determine new basic velocity
-	Vec3 current_vertical_velocity = Vec3(0, mCharacter->GetLinearVelocity().GetY(), 0);
+	Vec3 MyVelo = mCharacter->GetLinearVelocity();
+	Vec3 current_vertical_velocity = Vec3(0, MyVelo.GetY(), 0);
+	Vec3 current_planar_velocity = Vec3(MyVelo.GetX(), 0, MyVelo.GetZ());
 	Vec3 ground_velocity = mCharacter->GetGroundVelocity();
 	Vec3 new_velocity = mForcesUpdate;
 	mForcesUpdate = Vec3::sZero();
+	//the original design used ground velocity but because of our queuing design, we actually may often be in a state where
+	//things are known good but we don't have a ground velocity yet.
+	
 	//this ensures small forces won't knock us off the ground, vastly reducing jitter.
 	if (mCharacter->GetGroundState() == CharacterVirtual::EGroundState::OnGround // If on ground
 		&& (current_vertical_velocity.GetY() - ground_velocity.GetY()) < 0.3f)
 	// And not moving away from ground
 	{
-		// Assume velocity of ground when on ground
-		new_velocity += ground_velocity;
+		
+		if(ground_velocity.IsNearZero())
+		{//during initial settling, we want to maintain velocity.
+			new_velocity += current_planar_velocity;
+		}
+		else
+		{// Assume ground velocity when on ground for multiple ticks.
+			new_velocity += ground_velocity;
+		}
 	}
 	else
 	{
-		new_velocity += ground_velocity + current_vertical_velocity;
+		//allow total air control.
+		new_velocity += current_planar_velocity + current_vertical_velocity;
 	}
 
 	// Gravity
